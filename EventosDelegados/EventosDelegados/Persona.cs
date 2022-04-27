f9b6d99953bd9f0573dbbcc79c8c59e5c3d6800c
DAR-LAB-MA\UCSE <a>
2022-04-27 19:56:42 -0300

    Commit desde clases

---
+++ EventosDelegados/EventosDelegados/Persona.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventosDelegados
+{
+    public class NombreEventArgs : EventArgs
+    {
+        public string NombreAntiguo { get; set; }
+        public string NombreNuevo { get; set; }
+        public NombreEventArgs(string nombreAntiguo, string nombreNuevo)
+        {
+            NombreAntiguo = nombreAntiguo;
+            NombreNuevo = nombreNuevo;
+        }
+    }
+    public class Persona
+    {
+        private string Nombre { get; set; }
+        public EventHandler<NombreEventArgs> NombreModificado;
+        public bool ActualizacionNombre(string nuevoNombre)
+        {
+            string nombreAntiguo = this.Nombre;
+            Nombre = nuevoNombre;
+            this.NombreModificado(this, new NombreEventArgs(nombreAntiguo, Nombre));
+            return true;
+        }
+
+        public void setNombre(string nombre)
+        {
+            this.Nombre = nombre;
+        }
+
+    }
+}
---
+++ EventosDelegados/EventosDelegados/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventosDelegados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.setNombre("Juancito");//set nombreDefault
+
+
+            persona.ActualizacionNombre("Pepe");
+            persona.NombreModificado += Logger;
+
+
+
+        }
+        static void Logger(object sender, NombreEventArgs e)
+        {
+            Console.WriteLine($"El nombre fue modificado de {e.NombreAntiguo} a {e.NombreNuevo}");
+        }
+    }
+}
